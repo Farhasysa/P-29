cfe493c00a2a1f3b77de5cc75a4fecd96a350655
Farhasysa <83393361+Farhasysa@users.noreply.github.com>
2021-05-29 13:30:22 +0530

    Add files via upload

---
+++ polygon.js
@@ -0,0 +1,9 @@
+class Polygon{
+    constructor(x, y, width, height) {
+        var options = {
+                     
+        }
+        this.image = loadImage("sprites/polygon.png");
+    }
+  
+    display();
